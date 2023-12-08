17d18e3699d083044fd42383206ee13872f6ed18
riken95 <128236100+riken95@users.noreply.github.com>
2023-12-08 20:39:11 +0100

    Commit update ça marche

--- sbus_to_pwm_uno/sbus_to_pwm_uno.ino
+++ sbus_to_pwm_uno/sbus_to_pwm_uno.ino
@@ -5,59 +5,62 @@ uint8_t buf[26];
 char sbus_flag=0;
 const int minAngle = 30; // servo angle range
 const int maxAngle = 150;
-Servo servo1;
-Servo servo2;
-Servo servo3;
-Servo servo4;
-Servo servo5;
+int anglePrecedent = -1;
+
+//On crée une array contenant tous les servos
+Servo g_liste_servos[5];
+
 
 void setup()
 {   
-  servo1.attach(3);//servo pins
-  servo2.attach(5);
-  servo3.attach(6);
-  servo4.attach(9);
-  servo5.attach(10);
+  int liste_pins_servos[5] = {3,5,6,9,10};
+  int i(0);
+  //On assigne à chaque servo un pin
+  for(Servo &servo : g_liste_servos)
+    servo.attach(liste_pins_servos[i++]);
   Serial.begin(100000);
 }
 
-void setServoAngle(Servo s, int angle) {//mapping the channel value to pwm output for servos
-  int Angle = map(angle, 512, 1536, minAngle, maxAngle);
-  s.write(Angle);
+void setServoAngle(Servo s, const int angle) {//mapping the channel value to pwm output for servos
+  if(angle != anglePrecedent){
+    anglePrecedent = angle;
+    int Angle = map(angle, 512, 1536, minAngle, maxAngle);
+    s.write(Angle);
+  }
 }
 
-void moteur(int throttle) {//the mapping of throttle is different than servos
+void moteur(Servo &servo,const int throttle) {//the mapping of throttle is different than servos
   int rot = map(throttle, 512, 1536, 60, 130);
-  servo3.write(rot);
+  servo.write(rot);
 }
 
 void loop(){
   delay(5);
-  setServoAngle(servo1,CH[0]);
-  setServoAngle(servo2,CH[1]);
-  moteur(CH[2]);
-  setServoAngle(servo4,CH[3]);
-  setServoAngle(servo5,CH[4]);
+  int i(0);
+  for(Servo &servo : g_liste_servos)
+    if(i == 2)
+      moteur(servo,CH[i++]);
+    else
+      setServoAngle(servo,CH[i++]);
 }
 
 void serialEvent(void){//this function is activated by serial input, and saves sbus data in buf
-    if(Serial.available()>25){
-      while(sbus_flag==0){
-        buf[0]=Serial.read();
-        if(buf[0]==0x0f)//the sbus communication begins with 0x0f
-        {sbus_flag=1;}
-      }
-      if(sbus_flag==1){
-        for(int i=1;i<26;i++){
-          buf[i]=Serial.read();
-        }
-        sbus_flag=0;
-      }
-  }
+  //Gestion d'erreurs
+  if(Serial.available()<=25)
+    goto close_function;
+  
+  while(true)
+    if((buf[0]=Serial.read())==0x0f)//the sbus communication begins with 0x0f
+      break;
+  
+  for(int i=1;i<26;i++)
+    buf[i]=Serial.read();
+  
+  close_function:
   Sbus_Data_Count(buf);
 }
 
-void Sbus_Data_Count(uint8_t *buf){//function translating sbus data to integers
+void Sbus_Data_Count(const uint8_t *buf){//function translating sbus data to integers
   if(buf[0]==0x0f&&buf[24]==0x00){
     CH[ 0] = ((int16_t)buf[ 1] >> 0 | ((int16_t)buf[ 2] << 8 )) & 0x07FF;
     CH[ 1] = ((int16_t)buf[ 2] >> 3 | ((int16_t)buf[ 3] << 5 )) & 0x07FF;
